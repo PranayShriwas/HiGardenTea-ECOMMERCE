f4e54ed9eddac4fe775555181927f14db299c1e9
PranayShriwas <pranayshriwas420@gmail.com>
2023-05-18 20:34:04 +0530

    Complete

--- Ecommerce/ecom/urls.py
+++ Ecommerce/ecom/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
-    path('contact/', views.contact)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact),
+    path('infor/', views.infor, name='inquirey'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Ecommerce/ecom/views.py
+++ Ecommerce/ecom/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.http.response import HttpResponse
+from django.contrib import messages
 # Create your views here.
 
 
@@ -13,4 +15,17 @@ def about(request):
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    per = Person.objects.all()
+    return render(request, 'contact.html', {'per': per})
+
+
+def infor(request):
+    if request.method == 'POST':
+        nm = request.POST['name']
+        em = request.POST['email']
+        su = request.POST['subject']
+        me = request.POST['message']
+        Person.objects.create(name=nm, email=em, subject=su, message=me)
+        messages.success(
+            request, "Your feedback has been received, you will get a response soon.")
+        return redirect('/contact/')
